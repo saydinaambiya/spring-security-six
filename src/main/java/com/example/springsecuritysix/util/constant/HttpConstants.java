50ebf690e20c7484173e57edb7a97e8c7ed73779
saydinaambiya <saydinaambiya@hotmail.com>
2024-08-31 15:16:41 +0700

    add bearer and index token

--- src/main/java/com/example/springsecuritysix/util/constant/HttpConstants.java
+++ src/main/java/com/example/springsecuritysix/util/constant/HttpConstants.java
@@ -6,4 +6,6 @@ public final class HttpConstants {
     public static final String CONTENT_TYPE = "Content-Type";
     public static final String APPLICATION_JSON = "application/json";
     public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER = "Bearer ";
+    public static final int INDEX_TOKEN = 7;
 }
