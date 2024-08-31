a60d62539b6e4bcb06e20733e1b3cc0ca2cc210f
saydinaambiya <saydinaambiya@hotmail.com>
2024-08-31 15:08:15 +0700

    add http constants

---
+++ src/main/java/com/example/springsecuritysix/util/constant/HttpConstants.java
@@ -0,0 +1,9 @@
+package com.example.springsecuritysix.util.constant;
+
+public final class HttpConstants {
+    public HttpConstants() {}
+
+    public static final String CONTENT_TYPE = "Content-Type";
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+}
