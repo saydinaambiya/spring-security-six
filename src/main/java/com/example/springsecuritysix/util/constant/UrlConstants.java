ada6221442261dc9bb857b99cfa85bdc4491aa1f
saydinaambiya <saydinaambiya@hotmail.com>
2024-09-02 22:23:12 +0700

    add URL constants

---
+++ src/main/java/com/example/springsecuritysix/util/constant/UrlConstants.java
@@ -0,0 +1,10 @@
+package com.example.springsecuritysix.util.constant;
+
+public final class UrlConstants {
+    public UrlConstants() {}
+    public static final String BASE_URL = "/api/v1";
+    public static final String AUTHENTICATION_URL = BASE_URL + "/auth";
+    public static final String LOGIN = "/login";
+    public static final String LOGOUT = "/logout";
+    public static final String REGISTER = "/register";
+}
