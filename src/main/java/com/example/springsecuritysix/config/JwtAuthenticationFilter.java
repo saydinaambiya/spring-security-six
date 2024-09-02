861af31c6bcf5d065df417d3a12a2d87a3331c35
saydinaambiya <saydinaambiya@hotmail.com>
2024-09-02 22:24:07 +0700

    setup application configuration and jwt authentication filter

---
+++ src/main/java/com/example/springsecuritysix/config/JwtAuthenticationFilter.java
@@ -0,0 +1,52 @@
+package com.example.springsecuritysix.config;
+
+import com.example.springsecuritysix.secutity.service.JwtService;
+import com.example.springsecuritysix.util.constant.HttpConstants;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authorizationHeader = request.getHeader(HttpConstants.AUTHORIZATION_HEADER);
+        final String jwt;
+        final String userEmail;
+        if (authorizationHeader == null || !authorizationHeader.startsWith(HttpConstants.BEARER)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authorizationHeader.substring(HttpConstants.INDEX_TOKEN);
+        userEmail = jwtService.extractUsername(jwt);
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+            filterChain.doFilter(request, response);
+        }
+    }
+}
