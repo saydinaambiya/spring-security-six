3b127f05fa73106856583c217045327b9911f534
saydinaambiya <saydinaambiya@hotmail.com>
2024-09-02 22:22:19 +0700

    add whitelist endpoints for security config

--- src/main/java/com/example/springsecuritysix/util/constant/HttpConstants.java
+++ src/main/java/com/example/springsecuritysix/util/constant/HttpConstants.java
@@ -8,4 +8,5 @@ public final class HttpConstants {
     public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String BEARER = "Bearer ";
     public static final int INDEX_TOKEN = 7;
+    public static final String[] WHITELIST_ENDPOINTS = {UrlConstants.LOGIN, UrlConstants.REGISTER};
 }
