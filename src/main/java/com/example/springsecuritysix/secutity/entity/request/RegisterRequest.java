12ff3130f34a648faeb80f053f4b3ea4efffc550
saydinaambiya <ssyyddn@gmail.com>
2024-09-08 17:35:21 +0700

    create login and register request

---
+++ src/main/java/com/example/springsecuritysix/secutity/entity/request/RegisterRequest.java
@@ -0,0 +1,17 @@
+package com.example.springsecuritysix.secutity.entity.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String fullName;
+    private String username;
+    private String email;
+    private String password;
+}
