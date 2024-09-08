2f0949ab61bf3f4ae431f74a67a957fc43c2c2c9
saydinaambiya <ssyyddn@gmail.com>
2024-09-08 17:36:13 +0700

    mapping response code

---
+++ src/main/java/com/example/springsecuritysix/util/constant/ResponseCodeConstants.java
@@ -0,0 +1,16 @@
+package com.example.springsecuritysix.util.constant;
+
+public final class ResponseCodeConstants {
+    public ResponseCodeConstants() {}
+
+    // 2xx - Success
+
+    public static final String OK = "200";
+    public static final String CREATED = "201";
+
+    // 3xx - Redirection
+
+    // 4xx - Client Errors
+
+    // 5xx - Server Errors
+}
